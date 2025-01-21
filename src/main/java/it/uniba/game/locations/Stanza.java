dc346ab12cdd998ee254d864b8a1e3dc75030a6b
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-21 20:28:27 +0100

    Modifica classe Stanza

--- src/main/java/it/uniba/game/locations/Stanza.java
+++ src/main/java/it/uniba/game/locations/Stanza.java
@@ -1,9 +1,58 @@
 package it.uniba.game.locations;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Stanza {
-    private int id;
+    private String idCompleto;    // formato "01-07"
+    private String idStanza;      // formato "07"
     private String nome;
     private String descrizione;
     private boolean accessibile;
+    private Edificio edificio;
 
+
+
+    public Stanza(String idStanza, String nome, String descrizione, boolean accessibile, Edificio edificio) {
+        this.idStanza = idStanza;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.accessibile = accessibile;
+        this.edificio = edificio;
+        this.idCompleto = edificio.getId() + "-" + idStanza;
+    }
+
+    public String getIdCompleto() {
+        return idCompleto;
+    }
+
+    public String getIdStanza() {
+        return idStanza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public boolean isAccessibile() {
+        return accessibile;
+    }
+
+    public Edificio getEdificio() {
+        return edificio;
+    }
+
+    public void setAccessibile(boolean accessibile) {
+        this.accessibile = accessibile;
+    }
+
+    @Override
+    public String toString() {
+        return "Stanza " + idCompleto + ": " + nome +
+                (accessibile ? " (Accessibile)" : " (Non accessibile)");
+    }
 }
