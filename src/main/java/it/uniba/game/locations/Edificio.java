4f05dd809b06d629aab9192448e15caf8725d2d6
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-21 18:40:44 +0100

    Aggiunte classi Edificio e Stanza

---
+++ src/main/java/it/uniba/game/locations/Edificio.java
@@ -0,0 +1,14 @@
+package it.uniba.game.locations;
+
+public class Edificio {
+    private int id;
+    private String nome;
+    private boolean accessibile;
+
+    public Edificio(int id, String nome, boolean accessibile) {
+        this.id = id;
+        this.nome = nome;
+        this.accessibile = accessibile;
+    }
+
+}
---
+++ src/main/java/it/uniba/game/locations/Stanza.java
@@ -0,0 +1,9 @@
+package it.uniba.game.locations;
+
+public class Stanza {
+    private int id;
+    private String nome;
+    private String descrizione;
+    private boolean accessibile;
+
+}
