80ded84e8866e0bab9fbc05cf18bd1c13bcdff8d
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-21 20:29:31 +0100

    Aggiunta classe Direzione

---
+++ src/main/java/it/uniba/game/comandi/Direzione.java
@@ -0,0 +1,29 @@
+package it.uniba.game.comandi;
+
+public enum Direzione {
+    NORD(1),
+    SUD(2),
+    EST(3),
+    OVEST(4),
+    ALTO(5),
+    BASSO(6);
+
+    private int codice;
+
+    Direzione(int codice) {
+        this.codice = codice;
+    }
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public static Direzione getDaDirezione(int codice) {
+        for (Direzione d : values()) {
+            if (d.getCodice() == codice) {
+                return d;
+            }
+        }
+        return null;
+    }
+}
