8f5abc973eefb322b38e029b74160ef6ba2a3f5d
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-26 19:41:47 +0100

    Modificata la classe Giocatore: posizioneAttuale_edificio, posizioneAttuale_stanza

--- src/main/java/it/uniba/game/entity/Giocatore.java
+++ src/main/java/it/uniba/game/entity/Giocatore.java
@@ -4,20 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Giocatore {
-    private Stanza posizioneAttuale;
+    private Edificio posizioneAttuale_edificio;
+    private Stanza posizioneAttuale_stanza;
     private List<Oggetto> inventario;
 
-    public Giocatore(Stanza posizioneAttuale) {
-        this.posizioneAttuale = posizioneAttuale;
+    public Giocatore(Edificio posizioneAttuale_edificio, Stanza posizioneAttuale_stanza) {
+        this.posizioneAttuale_edificio = posizioneAttuale_edificio;
+        this.posizioneAttuale_stanza = posizioneAttuale_stanza;
         this.inventario = new ArrayList<>();
     }
 
-    public Stanza getPosizioneAttuale() {
-        return posizioneAttuale;
+    public Edificio getPosizioneAttuale_edificio() {
+        return posizioneAttuale_edificio;
     }
 
-    public void setPosizioneAttuale(Stanza posizioneAttuale) {
-        this.posizioneAttuale = posizioneAttuale;
+    public void setPosizioneAttuale_edificio(Edificio posizioneAttuale_edificio) {
+        this.posizioneAttuale_edificio = posizioneAttuale_edificio;
+    }
+
+    public Stanza getPosizioneAttuale_stanza() {
+        return posizioneAttuale_stanza;
+    }
+
+    public void setPosizioneAttuale(Stanza posizioneAttuale_stanza) {
+        this.posizioneAttuale_stanza = posizioneAttuale_stanza;
     }
 
     public List<Oggetto> getInventario() {
