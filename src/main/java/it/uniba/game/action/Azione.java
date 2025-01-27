0d410443398c0ff2dc6c2329cc418aa39d938176
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-27 09:39:03 +0100

    Aggiunta interfaccia Azione

---
+++ src/main/java/it/uniba/game/action/Azione.java
@@ -0,0 +1,15 @@
+package it.uniba.game.action;
+
+import it.uniba.game.entity.Giocatore;
+import java.util.List;
+
+public interface Azione {
+    /**
+     * Esegue l'azione specificata dal giocatore.
+     *
+     * @param giocatore Il giocatore che esegue l'azione.
+     * @param parametri Una lista di parametri per l'azione. (es. direzione o nome dell'oggetto).
+     * @return Una stringa che descrive il risultato dell'azione.
+     */
+    String esegui(Giocatore giocatore, List<String> parametri);
+}
