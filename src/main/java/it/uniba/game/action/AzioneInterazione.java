40552757cf4f571b17bae09194c97451feaf8b3a
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-28 21:08:09 +0100

    Modificato il file AzioneInterazione: aggiunti i metodi per la gestione delle azioni

--- src/main/java/it/uniba/game/action/AzioneInterazione.java
+++ src/main/java/it/uniba/game/action/AzioneInterazione.java
@@ -1,5 +1,6 @@
 package it.uniba.game.action;
 
+import it.uniba.game.database.dao.AzioneDAO;
 import it.uniba.game.entity.Giocatore;
 import it.uniba.game.database.dao.OggettoDAO;
 import it.uniba.game.entity.Oggetto;
@@ -10,9 +11,25 @@ import java.util.List;
 public class AzioneInterazione {
     private Connection connection;
     private OggettoDAO oggettoDAO;
+    private AzioneDAO azioneDAO;
+    private Oggetto oggetto;
 
-    public AzioneInterazione(Connection connection) {
-        this.connection = connection;
+    public AzioneInterazione() {
+    }
+
+    /**
+     * Esamina un oggetto nella stanza.
+     *
+     * @param giocatore Il giocatore che esegue l'azione.
+     * @param parametri Una lista di parametri per l'azione.
+     */
+    public Void esamina(Giocatore giocatore, List<String> parametri) {
+        if (azioneDAO.verificaAzioneInterazione(parametri.get(0), parametri.get(1), giocatore.getPosizioneAttualeId())) {
+            System.out.println(oggettoDAO.getDescrizioneDettagliataOggetto(parametri.get(1)));
+        } else {
+            System.out.println("Non puoi esaminare questo oggetto.");
+        }
+        return null;
     }
 
     /**
@@ -21,14 +38,16 @@ public class AzioneInterazione {
      * @param giocatore Il giocatore che esegue l'azione.
      * @param parametri Una lista di parametri per l'azione.
      */
-    public void prendi(Giocatore giocatore, List<String> parametri) {
-        if(oggettoDAO.isOggettoRaccoglibile(parametri.get(1), giocatore.getPosizioneAttualeId())) {
+    public Void prendi(Giocatore giocatore, List<String> parametri) {
+        if (oggettoDAO.isOggettoRaccoglibile(parametri.get(1), giocatore.getPosizioneAttualeId())) {
             Oggetto oggetto = oggettoDAO.getOggettoById(parametri.get(1));
             giocatore.aggiungiOggetto(oggetto);
+            oggettoDAO.eliminaOggettoDaStanza(parametri.get(1), giocatore.getPosizioneAttualeId());
             System.out.println("Hai aggiunto " + oggetto.getNome() + " all'inventario.");
         } else {
             System.out.println("Non puoi raccogliere questo oggetto.");
         }
+        return null;
     }
 
     /**
@@ -37,20 +56,89 @@ public class AzioneInterazione {
      * @param giocatore Il giocatore che esegue l'azione.
      * @param parametri Una lista di parametri per l'azione.
      */
-    public void lascia(Giocatore giocatore, List<String> parametri) {
+    public Void lascia(Giocatore giocatore, List<String> parametri) {
         Oggetto oggetto = giocatore.getInventario().stream()
                 .filter(o -> o.getOggettoId().equals(parametri.get(1)))
                 .findFirst()
                 .orElse(null);
 
-        if(oggetto != null) {
+        if (oggetto != null) {
             giocatore.rimuoviOggetto(oggetto);
             oggettoDAO.aggiungiOggettoAStanza(parametri.get(1), giocatore.getPosizioneAttualeId());
             System.out.println("Hai lasciato " + oggetto.getNome() + ".");
         } else {
-            System.out.println("Non hai questo oggetto nell'inventario.");
+            System.out.println("L'oggetto non è nell'inventario.");
         }
+        return null;
     }
 
+    /**
+     * Usa un oggetto dall'inventario.
+     *
+     * @param giocatore
+     * @param parametri
+     */
+    public Void usa(Giocatore giocatore, List<String> parametri) {
+        return null;
+    }
 
+    /**
+     * Apre un oggetto.
+     *
+     * @param giocatore
+     * @param parametri
+     */
+    public Void apri(Giocatore giocatore, List<String> parametri) {
+        if (azioneDAO.verificaAzioneInterazione(parametri.get(0), parametri.get(1), giocatore.getPosizioneAttualeId())) {
+            oggettoDAO.setVisibilitaOggetto("10", true);
+        } else {
+            System.out.println("Non puoi aprire questo oggetto.");
+        }
+        return null;
+    }
+
+    /**
+     * Chiude un oggetto.
+     *
+     * @param giocatore
+     * @param parametri
+     */
+    public Void chiudi(Giocatore giocatore, List<String> parametri) {
+        if (azioneDAO.verificaAzioneInterazione(parametri.get(0), parametri.get(1), giocatore.getPosizioneAttualeId())) {
+            oggettoDAO.setVisibilitaOggetto("10", false);
+        } else {
+            System.out.println("Non puoi chiudere questo oggetto.");
+        }
+        return null;
+    }
+
+    /**
+     * Parla con un personaggio nella stanza.
+     *
+     * @param giocatore
+     * @param parametri
+     */
+    public Void parla(Giocatore giocatore, List<String> parametri) {
+        if (azioneDAO.verificaAzioneInterazione(parametri.get(0), parametri.get(1), giocatore.getPosizioneAttualeId())) {
+            System.out.println(oggettoDAO.getDescrizioneDettagliataOggetto(parametri.get(1)));
+        } else {
+            System.out.println("Non puoi parlare con questo personaggio.");
+        }
+        return null;
+    }
+
+    /**
+     * Leggi un oggetto nella stanza.
+     *
+     * @param giocatore
+     * @param parametri
+     */
+    public Void leggi(Giocatore giocatore, List<String> parametri) {
+        if (azioneDAO.verificaAzioneInterazione(parametri.get(0), parametri.get(1), giocatore.getPosizioneAttualeId())) {
+            System.out.println(oggettoDAO.getDescrizioneDettagliataOggetto(parametri.get(1)));
+        } else {
+            System.out.println("Non puoi leggere questo oggetto.");
+        }
+        return null;
+    }
 }
