e82198e127eca6ef6f85cc2e92a517202b3b5e97
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-28 20:47:29 +0100

    Modificato il file AzioneMovimento

--- src/main/java/it/uniba/game/action/AzioneMovimento.java
+++ src/main/java/it/uniba/game/action/AzioneMovimento.java
@@ -3,21 +3,22 @@ package it.uniba.game.action;
 import it.uniba.game.entity.Giocatore;
 import it.uniba.game.entity.Stanza;
 import it.uniba.game.database.dao.MovimentoDAO;
-import it.uniba.game.database.DatabaseManager;
+import it.uniba.game.database.dao.AzioneDAO;
 import java.util.List;
 
 public class AzioneMovimento {
     private MovimentoDAO movimentoDAO; // DAO per i movimenti
+    private AzioneDAO azioneDAO; // DAO per le azioni
 
     public AzioneMovimento() {}
 
-    public void movimento(Giocatore giocatore, List<String> parametri) {
+    public Void movimento(Giocatore giocatore, List<String> parametri) {
         // Controlla se la direzione è valida
         if (parametri == null || parametri.isEmpty()) {
             System.out.println("Devi specificare una direzione valida.");
         }
 
-        String direzione = parametri.get(0).toLowerCase();
+        String direzione = azioneDAO.getNomeAzioneById(parametri.get(0)); ;
         Stanza stanzaAttuale = giocatore.getPosizioneAttuale();
 
         // Ottiene la stanza di arrivo per la direzione specificata
@@ -30,5 +31,6 @@ public class AzioneMovimento {
         } else {
             System.out.println("Non puoi andare in quella direzione.");
         }
+        return null;
     }
 }
