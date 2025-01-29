d5063e793ab7b87c31de6f8972d8d2a46211c0bf
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-29 23:27:50 +0100

    Modificata la classe Engine

--- src/main/java/it/uniba/game/Engine.java
+++ src/main/java/it/uniba/game/Engine.java
@@ -1,10 +1,59 @@
 package it.uniba.game;
 
-import it.uniba.game.util.CSVLoader;
+import it.uniba.game.action.GestoreAzioni;
+import it.uniba.game.database.DatabaseInitializer;
+import it.uniba.game.entity.Giocatore;
+import it.uniba.game.util.GUI;
+import it.uniba.game.util.Parser;
+import javax.swing.*;
+import java.util.List;
+
 
 public class Engine {
+    private GestoreAzioni gestoreAzioni;
+    private Giocatore giocatore;
+    private Parser parser;
+    private GUI gui;
+
+    public Engine() {
+        try {
+            // Inizializzazione del database
+            DatabaseInitializer.initializeDatabase();
+
+            gestoreAzioni = new GestoreAzioni();
+            giocatore = new Giocatore();
+            parser = new Parser();
+            // Inizializza e avvia la GUI nel costruttore
+            SwingUtilities.invokeLater(() -> {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                gui = new GUI(this); // Passa l'istanza di Engine alla GUI
+                gui.setVisible(true);
+
+            });
+        } catch (Exception e) {
+            System.err.println("Errore durante l'inizializzazione del gioco:");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public Object processCommand(String input) {
+        List<String> parametri = parser.parseInput(input);
+        if (parametri.isEmpty()) {
+            return "Comando non riconosciuto!";
+        }
+
+        return gestoreAzioni.esegui(giocatore, parametri);
+    }
+
     public static void main(String[] args) {
-        String filePath = "src/main/resources/edifici.csv"; // Percorso del file CSV
-        CSVLoader.loadEdificiFromCSV(filePath);
+        new Engine(); // Crea un'istanza di Engine per avviare il gioco
+
     }
 }
