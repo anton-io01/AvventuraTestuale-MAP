92f08601663a358b46542f11cee9d9a2ad928e51
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-30 11:59:46 +0100

    Modificata la classe Giocatore

--- src/main/java/it/uniba/game/entity/Giocatore.java
+++ src/main/java/it/uniba/game/entity/Giocatore.java
@@ -1,19 +1,15 @@
 package it.uniba.game.entity;
 
-import it.uniba.game.database.dao.StanzaDAO;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Giocatore {
     private Stanza posizioneAttuale;
-    private StanzaDAO stanzaDAO = new StanzaDAO();
     private List<Oggetto> inventario;
 
     public Giocatore() {
-        this.posizioneAttuale = stanzaDAO.getStanzaById("02");
+        this.posizioneAttuale = null;
         this.inventario = new ArrayList<>();
-        this.inventario.add(new Oggetto("01","Flipper Zero"));
     }
 
     public Stanza getPosizioneAttuale() {
