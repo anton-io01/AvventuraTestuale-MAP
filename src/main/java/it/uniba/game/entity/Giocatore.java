0583820d366f488014fccb739be16c027fb3f21b
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-30 15:57:12 +0100

    Modificata la classe Giocatore

--- src/main/java/it/uniba/game/entity/Giocatore.java
+++ src/main/java/it/uniba/game/entity/Giocatore.java
@@ -3,36 +3,49 @@ package it.uniba.game.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Giocatore {
+
+public class Giocatore{
+
     private Stanza posizioneAttuale;
     private List<Oggetto> inventario;
 
-    public Giocatore() {
-        this.posizioneAttuale = null;
+    public Giocatore(){
         this.inventario = new ArrayList<>();
+    }
+    public void aggiungiOggetto(Oggetto oggetto){
+        this.inventario.add(oggetto);
+    }
+
+    public void rimuoviOggetto(Oggetto oggetto){
+        this.inventario.remove(oggetto);
     }
 
     public Stanza getPosizioneAttuale() {
         return posizioneAttuale;
     }
 
+    public String getPosizioneAttualeId(){ return this.posizioneAttuale.getStanzaId();};
     public void setPosizioneAttuale(Stanza posizioneAttuale) {
         this.posizioneAttuale = posizioneAttuale;
-    }
-
-    public String getPosizioneAttualeId() {
-        return posizioneAttuale.getStanzaId();
     }
 
     public List<Oggetto> getInventario() {
         return inventario;
     }
-
-    public void aggiungiOggetto(Oggetto oggetto) {
-        inventario.add(oggetto);
+    public String getPlayerParams() {
+        StringBuilder inventarioString = new StringBuilder();
+        for (int i = 0; i < inventario.size(); i++) {
+            inventarioString.append(inventario.get(i).getOggettoId());
+            if (i < inventario.size() - 1) {
+                inventarioString.append(",");
+            }
+        }
+        if (posizioneAttuale != null) {
+            return posizioneAttuale.getStanzaId() + ";" + inventarioString.toString();
+        }else{
+            return "";
+        }
     }
 
-    public void rimuoviOggetto(Oggetto oggetto) {
-        inventario.remove(oggetto);
-    }
+
 }
