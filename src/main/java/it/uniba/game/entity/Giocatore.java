a732561f65aad1a13baac5c07bf91fab59a13da2
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-30 10:58:14 +0100

    Modificata classe Giocatore: inizializzazione Stanza posizione attuale

--- src/main/java/it/uniba/game/entity/Giocatore.java
+++ src/main/java/it/uniba/game/entity/Giocatore.java
@@ -1,14 +1,17 @@
 package it.uniba.game.entity;
 
+import it.uniba.game.database.dao.StanzaDAO;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Giocatore {
     private Stanza posizioneAttuale;
+    private StanzaDAO stanzaDAO = new StanzaDAO();
     private List<Oggetto> inventario;
 
     public Giocatore() {
-        this.posizioneAttuale = new Stanza("01","02","ingresso");
+        this.posizioneAttuale = stanzaDAO.getStanzaById("02");
         this.inventario = new ArrayList<>();
         this.inventario.add(new Oggetto("01","Flipper Zero"));
     }
