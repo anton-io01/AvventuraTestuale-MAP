1e7c8342fe3a6faedf8c9bfa411045ac51bf11fe
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-30 16:38:12 +0100

    Modificata la classe Engine

--- src/main/java/it/uniba/game/Engine.java
+++ src/main/java/it/uniba/game/Engine.java
@@ -7,41 +7,56 @@ import it.uniba.game.database.DatabaseInitializer;
 import it.uniba.game.entity.Giocatore;
 import it.uniba.game.util.GUI;
 import it.uniba.game.util.Parser;
+
 import javax.swing.*;
 import java.util.List;
 
-
 public class Engine {
+
     private GestoreAzioni gestoreAzioni;
     private Giocatore giocatore;
     private Parser parser;
     private GUI gui;
+    private StanzaDAO stanzaDAO;
+    private OggettoDAO oggettoDAO;
 
     public Engine() {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            startGame();
+        });
+    }
+
+    private void startGame() {
         try {
             // Inizializzazione del database
             DatabaseInitializer.initializeDatabase();
 
-            StanzaDAO stanzaDAO = new StanzaDAO();
-            OggettoDAO oggettoDAO = new OggettoDAO();
 
+            //istanzio i DAO
+            stanzaDAO = new StanzaDAO();
+            oggettoDAO = new OggettoDAO();
             gestoreAzioni = new GestoreAzioni();
-            giocatore = new Giocatore();
-            giocatore.setPosizioneAttuale(stanzaDAO.getStanzaById("02"));
-            giocatore.aggiungiOggetto(oggettoDAO.getOggettoById("01"));
-            parser = new Parser();
-            // Inizializza e avvia la GUI nel costruttore
-            SwingUtilities.invokeLater(() -> {
-                try {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-                gui = new GUI(this); // Passa l'istanza di Engine alla GUI
-                gui.setVisible(true);
 
-            });
+
+            //Creazione del giocatore di default all'avvio del game.
+            giocatore = new Giocatore();
+            //Imposto posizione iniziale
+            if(giocatore.getPosizioneAttuale()==null) { // set posizione default / lista oggetti
+                giocatore.setPosizioneAttuale(stanzaDAO.getStanzaById("02"));
+                giocatore.aggiungiOggetto(oggettoDAO.getOggettoById("01"));
+            }
+            parser = new Parser();
+            // Inizializza e avvia la GUI passando il giocatore come argomento.
+            gui = new GUI(this);  // Rimozione giocatore dal costruttore
+
+            gui.setVisible(true);
+
+
         } catch (Exception e) {
             System.err.println("Errore durante l'inizializzazione del gioco:");
             e.printStackTrace();
@@ -49,18 +64,16 @@ public class Engine {
 
     }
 
-
     public Object processCommand(String input) {
         List<String> parametri = parser.parseInput(input);
         if (parametri.isEmpty()) {
             return "Comando non riconosciuto!";
         }
-
         return gestoreAzioni.esegui(giocatore, parametri);
     }
 
-    public static void main(String[] args) {
-        new Engine(); // Crea un'istanza di Engine per avviare il gioco
 
+    public static void main(String[] args) {
+        new Engine();
     }
 }
