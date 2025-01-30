9ab9099c53a0cf3d85836265a520736fd3a4a4d2
anton-io01 <antonio.2001.modugno@gmail.com>
2025-01-30 10:56:50 +0100

    Modificata classe AzioneMovimento: per movimenti tra edifici

--- src/main/java/it/uniba/game/action/AzioneMovimento.java
+++ src/main/java/it/uniba/game/action/AzioneMovimento.java
@@ -2,6 +2,7 @@ package it.uniba.game.action;
 
 import it.uniba.game.entity.Giocatore;
 import it.uniba.game.entity.Stanza;
+import it.uniba.game.entity.Edificio;
 import it.uniba.game.database.dao.MovimentoDAO;
 import it.uniba.game.database.dao.AzioneDAO;
 import it.uniba.game.database.dao.StanzaDAO;
@@ -16,6 +17,7 @@ public class AzioneMovimento {
     public AzioneMovimento() {}
 
     public String movimento(Giocatore giocatore, List<String> parametri) {
+        Edificio edificio;
         // Controlla se la direzione è valida
         if (parametri == null || parametri.isEmpty()) {
             return "Devi specificare una direzione valida.\n\n";
@@ -28,9 +30,20 @@ public class AzioneMovimento {
         Stanza stanzaDiArrivo = movimentoDAO.getStanzaDiArrivo(stanzaAttuale.getStanzaId(), direzione);
 
         if (stanzaDiArrivo != null) {
+            if(!stanzaAttuale.getEdificioId().equals(stanzaDiArrivo.getEdificioId())) {
+                edificio = stanzaDAO.getEdificioByStanza(stanzaDiArrivo.getStanzaId());
+                return edificio.getDescrizione() + "\n\n" +
+                        "Ti sei spostato verso " + direzione + " nella stanza: " + stanzaDiArrivo.getNome() + ".\n" + stanzaDAO.getDescrizioneCompleta(stanzaDiArrivo.getStanzaId()) + "\n\n";
+            }
             // Aggiorna la posizione del giocatore
             giocatore.setPosizioneAttuale(stanzaDiArrivo);
-            return "Ti sei spostato verso " + direzione + " nella stanza: " + stanzaDiArrivo.getNome() + ".\n" + stanzaDAO.getDescrizioneCompleta(stanzaDiArrivo.getStanzaId()) + "\n\n";
+            if (stanzaDiArrivo.getStanzaId().equals("01") || stanzaDiArrivo.getStanzaId().equals("15") || stanzaDiArrivo.getStanzaId().equals("22")) {
+                edificio = stanzaDAO.getEdificioByStanza(stanzaDiArrivo.getStanzaId());
+                String descrizione = "Ti sei spostato verso " + direzione + " all'esterno dell'edificio " + edificio.getNome() + ".\n" + movimentoDAO.getMovimentiByStanza(stanzaDiArrivo) + "\n";
+                return descrizione;
+            } else {
+                return "Ti sei spostato verso " + direzione + " nella stanza: " + stanzaDiArrivo.getNome() + ".\n" + stanzaDAO.getDescrizioneCompleta(stanzaDiArrivo.getStanzaId()) + "\n\n";
+            }
         } else {
             return "Non puoi andare in quella direzione.\n\n";
         }
